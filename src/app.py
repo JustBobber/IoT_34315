1656952ab43a67b956d5dde17df44d3654d713c4
Bobber <bobberen@protonmail.com>
2026-04-23 18:37:27 +0200

    added helper functions

--- src/app.py
+++ src/app.py
@@ -15,6 +15,13 @@ app_state = {"user_id": None}  # bruges så vi kan sende user til esp'en så den
 
 
 def calculate_duration(start_time, end_time):
+    """
+    Hjælpe funktion der:
+    Tager et start og slut tidspunkt og beregner tiden i mellem de to.
+    :param start_time: Sql timestamp
+    :param end_time: Sql timestamp
+    :return: formateret string med duration mellem de to tider i minutter og sekunder
+    """
     start = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end = datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
     delta = end - start
@@ -22,6 +29,25 @@ def calculate_duration(start_time, end_time):
     seconds = delta.seconds % 60
 
     return f"{minutes}m {seconds}s"
+
+def get_duration_sum(sessions):
+    """
+    Hjælpe funktion der:
+    Tager en liste af sessions og beregner den totale varighed af sessionerne ved at summere varigheden af hver session.
+    :param sessions: Liste af objekter der indeholder obj["start_time"] og obj["end_time"]
+    :return: Formateret string der angiver sessioners totale varighed i formatet "<timer> t <minutter> m <sekunder> s"
+    """
+    total_seconds = 0
+    for s in sessions:
+        if s["start_time"] and s["end_time"]:
+            start = datetime.strptime(s["start_time"], "%Y-%m-%d %H:%M:%S")
+            end = datetime.strptime(s["end_time"], "%Y-%m-%d %H:%M:%S")
+            total_seconds += (end - start).total_seconds()
+
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds % 3600) // 60)
+    seconds = int(total_seconds % 60)
+    return f"{hours}t {minutes}m {seconds}s"
 
 
 @app.before_request
