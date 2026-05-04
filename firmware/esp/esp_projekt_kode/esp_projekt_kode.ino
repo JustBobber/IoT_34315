692f9c51e85c831a4e5f6a84fe78dc2c8137ed06
Bobber <bobberen@protonmail.com>
2026-05-04 16:55:34 +0200

    added units to display strings

--- firmware/esp/esp_projekt_kode/esp_projekt_kode.ino
+++ firmware/esp/esp_projekt_kode/esp_projekt_kode.ino
@@ -68,7 +68,6 @@ String session_uuid = "";
 bool user_logged_in = false;
 
 
-
 void updateDisplay(String tekst, int size = 10);
 
 void setup() {
@@ -152,7 +151,7 @@ void loop() {
     if (digitalRead(STOP_BUTTON_PIN) == LOW && session_in_progress) {
         bool result = stopSession(session_uuid);
         session_in_progress = !result;  // opdatere session state ud fra return af stop_session.
-        updateDisplay("Session has ended\n\nMax distance for session was: " + String((float)max_distance / 10));
+        updateDisplay("Session has ended\n\nMax distance for session was: " + String((float)max_distance / 10) + " cm");
     }
 
     if (session_in_progress == true) {
@@ -167,11 +166,11 @@ void loop() {
             if (tof.timeoutOccurred()) {
                 updateDisplay("Fejl: Kunne ikke læse afstands sensor!");
             } else {
-                updateDisplay("Distance: " + String((float)tofDistance / 10) +" (some unit)");
+                updateDisplay("Distance: " + String((float)tofDistance / 10) + " cm");
                 bool sendDataResult = send_session_data();
 
                 if (sendDataResult == true) {
-                    updateDisplay("Session in prograss\nDistance: " + String((float)tofDistance / 10, 2) + "\n\nMax distance: " + String((float)max_distance / 10, 2) + "\n Difficulty: " + String(difficulty));
+                    updateDisplay("Session in prograss\nDistance: " + String((float)tofDistance / 10, 2) + " cm" + "\n\nMax distance: " + String((float)max_distance / 10, 2) + " cm" + "\n Difficulty: " + String(difficulty));
                 } else {
                     updateDisplay("kunne ikke sende data!");
                 }
